6e1f047044ef3d61c57d8c0238734db4497f9907
Helder Eijs <helderijs@gmail.com>
2019-06-04 15:30:13 +0200

    First version

--- lib/Crypto/SelfTest/Cipher/test_ChaCha20.py
+++ lib/Crypto/SelfTest/Cipher/test_ChaCha20.py
@@ -243,6 +243,26 @@ class ChaCha20Test(unittest.TestCase):
             assert(ct == ct_expect)
 
 
+class XChaCha20Test(unittest.TestCase):
+
+    def test_hchacha20(self):
+
+        from Crypto.Cipher.ChaCha20 import _HChaCha20
+
+        key = b"00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f"
+        key = unhexlify(key.replace(":", ""))
+
+        nonce = b"00:00:00:09:00:00:00:4a:00:00:00:00:31:41:59:27"
+        nonce = unhexlify(nonce.replace(":", ""))
+
+        subkey = _HChaCha20(key, nonce)
+
+        expected = b"82413b42 27b27bfe d30e4250 8a877d73 a0f9e4d5 8a74a853 c12ec413 26d3ecdc"
+        expected = unhexlify(expected.replace(" ", ""))
+
+        self.assertEqual(subkey, expected)
+
+
 class ByteArrayTest(unittest.TestCase):
     """Verify we can encrypt or decrypt bytearrays"""
 
@@ -439,6 +459,7 @@ class TestOutput(unittest.TestCase):
 def get_tests(config={}):
     tests = []
     tests += list_test_cases(ChaCha20Test)
+    tests += list_test_cases(XChaCha20Test)
     tests.append(ChaCha20_AGL_NIR())
     tests.append(ByteArrayTest())
 
